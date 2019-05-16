98acf8e1378a20e597e7b4689b102d5165ba26b6
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-05-16 18:29:35 -0400

    working spotify

--- keys.js
+++ keys.js
@@ -2,6 +2,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: af6be61846a8467ba6c6063cb0c7a690,
-  secret: a47af44eb3484c59af43fc9bb0806301
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,6 +1,43 @@
 require("dotenv").config();
-var Spotify = require("node-spotify-api")
 
+var Spotify = require("node-spotify-api");
+var axios = require('axios');
 var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
+var searchType = process.argv[2]
+let temp = process.argv.splice(0,3)
+var searchTerm = process.argv.join(" ")
+
+
+switch(searchType) {
+
+	case "spotify-this-song":
+		spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function(err, response) {
+			if (err) { return console.log('Error occurred: ' + err);}
+
+			let tracks = response.tracks.items[0]
+			let artistArr = []
+
+			for (let i = 0; i < tracks.artists.length; i++) {
+				artistArr.push(tracks.artists[i].name)
+			}
+
+
+			console.log(`\ntrack title: ${tracks.name}`)
+			console.log(`track artist(s): ${artistArr}`)
+			console.log(`track album: ${tracks.album.name}`)
+			console.log(`track preivew URL: ${tracks.external_urls.spotify}\n`)
+		});
+		break
+	
+	case "movie-this":
+		axios.get(`http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&apikey=trilogy`).then(
+			function(response) {
+				console.log(response.data);
+			}
+		);
+		break
+}
+
 
